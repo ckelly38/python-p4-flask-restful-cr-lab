42811b7f4c7f85578b7fe26a807bad61e3aa5e8c
Christopher Hugh Kelly <christopher.kelly1997@gmail.com>
2024-01-07 02:01:33 -0700

    this demonstrates getting all data, getting a specific item, and posting an item to the server and adding it to the database, but there seems to be a minor proxy error

--- server/app.py
+++ server/app.py
@@ -17,11 +17,39 @@ db.init_app(app)
 api = Api(app)
 
 class Plants(Resource):
-    pass
+    def get(self):
+        return make_response([pt.to_dict() for pt in Plant.query.all()], 200);
+
+    def post(self):
+        #get the data and create the new instance/object
+        #then add this to the db
+        #then return the response
+        #print(request.form);
+        #print(request.json);
+        pt = None;
+        if (len(request.form) < 1):
+            pt = Plant(name=request.json.get("name"), image=request.json.get("image"),
+                       price=float(request.json.get("price")));
+        else:
+            pt = Plant(name=request.form.get("name"), image=request.form.get("image"),
+                       price=float(request.form.get("price")));
+        #print(pt);
+        #print(pt.to_dict());
+        db.session.add(pt);
+        db.session.commit();
+        return make_response(pt.to_dict(), 201);
+        #return make_response("NOT DONE YET 1-7-2024 1:20 AM", 406);
+
+api.add_resource(Plants, "/plants");
 
 class PlantByID(Resource):
-    pass
-        
+    def get(self, id):
+        plt = Plant.query.filter_by(id=id).first();
+
+        if (plt == None): return make_response(f"Plant with id {id} not found!", 404);
+        else: return make_response(plt.to_dict(), 200);
+
+api.add_resource(PlantByID, "/plants/<int:id>");       
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
